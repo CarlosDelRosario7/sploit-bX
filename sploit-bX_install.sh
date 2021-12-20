9ab169506dc889858c060a2090a76aa34aca0726
DannyHack <65183782+CarlosDelRosario7@users.noreply.github.com>
2021-12-20 18:17:05 +0000

    Add files via upload

---
+++ sploit-bX_install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#Actor: DannyHack
+
+export DEBIAN_FRONTEND=noninteractive
+
+#Mail
+
+apt update
+apt upgrade -y
+apt install python2.7 -y
+apt install curl -y
+curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py
+python2.7 get-pip.py
+apt install libssl-dev -y
+apt install libffi-dev -y
+apt install build-essential -y
+python2.7 -m pip install --upgrade setuptools
+python2.7 -m pip install 'pwntools==3.13.0'
+apt install bluetooth libbluetooth-dev -y
+apt install python2-dev -y
+python2.7 -m pip install 'pybluez==0.22'
+apt install git -y
+git clone https://github.com/ojasookert/CVE-2017-0781
+git clone https://github.com/ojasookert/CVE-2017-0785
+cd CVE-2017-0781
+echo -e "#! /usr/bin/python2.7\n$(cat CVE-2017-0781.py)" > sploit-b1
+chmod +x sploit-b1
+cp sploit-b1 /usr/bin
+cd ../CVE-2017-0785
+echo -e "#! /usr/bin/python2.7\n$(cat CVE-2017-0785.py)" > sploit-b5
+chmod +x sploit-b5
+cp sploit-b5 /usr/bin
+
+echo "[+] Listo"
+sleep 2
